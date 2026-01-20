603dc5b5bb28bcdd479278f3f3287d78fe6a0ba9
kk-Syuer <liyu.jin03@gmail.com>
2026-01-20 23:26:29 +0100

    deleted useless periodic republish of same task points

--- src/coverage_planner/src/coverage_planner_node.cpp
+++ src/coverage_planner/src/coverage_planner_node.cpp
@@ -698,9 +698,7 @@ private:
   // Main timer: deploy once and later react to routing changes.
   void periodicUpdate()
   {
-    if (!task_points_.empty()) {
-      publishTaskPoints();
-    }
+
     // First time: wait for status discovery then compute full deployment
     if (!first_deployment_done_) {
       if (!readyForDeployment()) {
